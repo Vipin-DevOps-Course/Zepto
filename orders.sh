2bbfdc14114abbc65bb6f8f793aada14d9cc542f
vipanchith-nalimela <vipanchith.nalimela@gmail.com>
2022-10-12 15:47:36 -0500

    added payments and orders

---
+++ orders.sh
@@ -0,0 +1,7 @@
+Customer Name                             Order Number
+John                                       2177865
+David                                      2174569
+Smith                                      3678798
+James                                      6573940
+Greg                                       3463504
+Roman                                      3475950
---
+++ payments.sh
@@ -0,0 +1,5 @@
+Bank Name                                   Amount
+chase                                       21,000
+BOFA                                        13,000
+DCU                                         16,000
+Wells fargo                                 19,000
