11fc6114d0ae0deda3da624b73168ab0a29c597c
vipanchith-nalimela <vipanchith.nalimela@gmail.com>
2022-10-15 22:27:10 -0500

    added discover

--- payments.sh
+++ payments.sh
@@ -4,4 +4,4 @@ BOFA                                        13,000
 DCU                                         16,000
 Wells fargo                                 19,000
 Amex					    11,000
-
+Discover                                    25.000
